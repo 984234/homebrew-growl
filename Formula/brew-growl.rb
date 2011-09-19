8b08042c958d40d4b89fdb06a603424fded9aec9
Second Planet <contact@secondplanetanimation.com>
2011-09-19 18:46:49 -0400

    md5

--- Formula/brew-growl.rb
+++ Formula/brew-growl.rb
@@ -4,7 +4,7 @@ class BrewGrowl < Formula
   url 'https://github.com/secondplanet/brew-growl/tarball/master'
   homepage 'https://github.com/secondplanet/brew-pip'
   version '0.0.1'
-  md5 '8f20f14dac46ad4298847af9545bc01c'
+  md5 '0aad2d10ffea6a57d16bcdf4cb2febc2'
 
   def install
     inreplace 'brew-growl.rb', /^BREW_PREFIX = '.*'$/, "BREW_PREFIX = '#{HOMEBREW_PREFIX}'"
